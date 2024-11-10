d95e1f6847416cb6b0245d94e9f0ea1f95fa2563
AcidJoshua <jimmy.jpl602@gmail.com>
2024-11-10 12:12:23 -0600

    FactorialDeUnNumero Commit

---
+++ src/factorialdeunnumero/Factorialdeunnumero.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package factorialdeunnumero;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jcplo
+ */
+public class Factorialdeunnumero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Scanner teclado = new Scanner(System.in);
+     System.out.print("Ingresar número: ");
+     int numero = scanner.nextInt();
+     if (numero < 0) {
+     System.out.println("El factorial no está definido.");
+     } else {
+     calcularFactorial(numero);
+     System.out.println("El factorial de " + numero + " es: " + resultadoFactorial);
+        }
+    }
+    }
+
